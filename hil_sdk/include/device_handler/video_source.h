cd8b22b98a1bde5c0ebdb1e4f8b2b3b5a28454ae
jimmy.xie <jimmy-ASUS-ubuntu18@ailiteam.com>
2023-11-25 17:39:35 +0800

    feat(hil_sdk): ALL NEW INTERFACE - DEVICE HANDLER

---
+++ hil_sdk/include/device_handler/video_source.h
@@ -0,0 +1,94 @@
+/*
+ The MIT License (MIT)
+
+Copyright (c) 2022 Aili-Light. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef __VIDEO_SOURCE_H__
+#define __VIDEO_SOURCE_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <semaphore.h>
+#include "alg_common/basic_types.h"
+
+struct VideoSourceParam
+{
+    int         source_id;
+    int         image_width;
+    int         image_height;
+    int         channel_id;
+    float       fps;
+
+    std::string source_type;
+    std::string source_name;
+    std::string output_type;
+
+    std::string config_file;
+};
+
+class VideoSource
+{
+protected:
+    VideoSource  ();
+public:
+    virtual ~VideoSource ();
+public:
+    void                    SetSourceName(const char* _source_name) {m_source_name=_source_name;};
+    void                    SetFrameRate(const float _fps) {m_fps=_fps;};
+    void                    SetChannelID(const int _ch_id) {m_ch_id=_ch_id;};
+    void                    SetOutputType(const char* _output_type) {m_output_type=_output_type;};
+protected:           
+    virtual int             InitVideoSource();
+    virtual int             OpenVideoSource();
+    virtual void            StartSource();
+    virtual void            EndSource();
+    virtual void            Loop();
+    virtual void            WaitForFinish();
+    virtual void            FreeVideoSource();
+    virtual bool            StreamOn(bool b_on);
+public:           
+    int                     GetChannelId()const{ return m_ch_id; }
+    int                     GetWidth()const{ return m_image_width; }
+    int                     GetHeight()const{ return m_image_height; }
+    float                   GetFrameRate()const{ return m_fps; }
+
+protected:
+    int                     m_image_width;
+    int                     m_image_height;
+    float                   m_fps;
+    int                     m_ch_id;
+    bool                    m_thread_started;
+
+    std::string             m_source_name;
+    std::string             m_output_type;
+    std::string             m_source_type;
+    std::string             m_bayer_type;
+
+    std::thread             m_thr;
+    std::mutex              m_mutex;
+    sem_t                   full;
+    sem_t                   empty;
+};
+
+
+#endif
---
+++ src/c/demo/demo_device_fromDir.cpp
@@ -0,0 +1,78 @@
+/*
+ The MIT License (MIT)
+
+Copyright (c) 2022 Aili-Light. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "device_handler/HIL_device_fromDir.h"
+
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        const uint16_t num_channel = atoi(argv[1]);
+        printf("num channels : %d\n", num_channel);
+        if (num_channel < 1)
+        {
+            fprintf(stderr, "ERROR! Number of Channel is incorrect!");
+            exit(1);
+        }
+
+        HILDeviceFromDir *hil_device = HILDeviceFromDir::GetInstance();
+
+        for (int i = 0; i < num_channel; i++)
+        {
+            const char *config_file = argv[2];
+            VideoSourceParam param;
+            param.source_id = i;
+            param.config_file = config_file;
+
+            hil_device->RegisterDevice(param);
+        }
+        if(!hil_device->Init())
+        {
+            fprintf(stderr, "Init device failed");
+            exit(1);
+        }
+
+        // Start Stream
+        hil_device->StartStreamAll();
+
+        // Main Loop
+        while(1)
+        {
+            usleep(100);
+        }
+
+        // Wait Until Stream Finish
+        hil_device->Wait();
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s <num_channel> <config_file>", argv[0]);
+        exit(0);
+    }
+
+    return 0;
+}
