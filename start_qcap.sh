3d79ff786e8407dcc63bdd41635e5b01124efa62
jimmy.xie <jimmy@ailiteam.com>
2023-11-25 18:17:42 +0800

    fix(hil_sdk): fix qcap start script

--- start_qcap.sh
+++ start_qcap.sh
@@ -3,4 +3,4 @@ dir=`pwd`
 export LD_LIBRARY_PATH=$dir/qcap_lib/lib:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 
-./release/linux/bin/hil_sdk_demo_push_qcap --qcap 3840 2160 1
+./release/linux/bin/hil_sdk_demo_device_fromQCap 1 '/home/test/hil_sdk_demo/config/hil_config_fromQCAP.json' 
